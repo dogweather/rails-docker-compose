4124a818a3ce9f588d4a4e6ff84fa6f4ceb31769
Robb Shecter <robb@public.law>
2018-02-02 20:17:45 -0800

    Initial checkin

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+# Ruby on Rails Development Environment
+
+FROM ruby:2.5.0
+LABEL maintainer="robb@public.law"
+
+# Set up Linux
+RUN apt-get update
+RUN apt-get install -y build-essential inotify-tools libpq-dev nodejs postgresql-client
+
+WORKDIR /app
+EXPOSE 3000
---
+++ run.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -e
+
+# Ensure the app's dependencies are installed
+echo "bundle install --without=production..."
+bundle install --without=production
+
+# Wait for Postgres to become available.
+echo "Checking for Postgres..."
+until psql -h db -U "postgres" -c '\q' 2>/dev/null; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+echo "Postgres is available: continuing with database setup..."
+
+# Potentially Set up the database
+echo "bundle exec rake db:setup..."
+bin/rails db:setup
+
+# Start the web server
+echo "bin/rails s -p 3000 -b '0.0.0.0'..."
+bin/rails s -p 3000 -b '0.0.0.0'
